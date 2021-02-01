fc2a3d46e88ae4b32e054e9c350347da35cabf2f
Saptadeep <saptadeep.dutta@gmail.com>
2021-02-01 23:15:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ ___
 
 """
 
+### The code that makes it work is here:
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
